bd8014440df93a1b7b1c5959d59aac99475c7658
Ted <my.koh@gabia.com>
2025-03-26 14:48:26 +0900

    feat: #3 'Role' entity 서식 변경

--- domain/role.py
+++ domain/role.py
@@ -9,10 +9,10 @@ from infrastructure.database import Base
 class Role(Base):
     __tablename__ = "role"
 
-    id: Mapped[str] = mapped_column("id", String(255), nullable = False, primary_key = True)
-    name: Mapped[str] = mapped_column("name", String(255), nullable = False)
-    created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable = False, default = datetime.now)
+    id: Mapped[str] = mapped_column("id", String(255), nullable=False, primary_key=True)
+    name: Mapped[str] = mapped_column("name", String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable=False, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(
-        "updated_at", DateTime, nullable = False, default=datetime.now, onupdate=datetime.now
+        "updated_at", DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
     )
-    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable = True, default = None)
+    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable=True, default=None)
