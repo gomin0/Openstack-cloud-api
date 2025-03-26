4af7754f06c81748f72be71f795f794bf93cbce3
Ted <my.koh@gabia.com>
2025-03-26 14:47:30 +0900

    feat: #3 'Project' entity FK 추가

--- domain/project.py
+++ domain/project.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from infrastructure.database import Base
@@ -9,10 +9,11 @@ from infrastructure.database import Base
 class Project(Base):
     __tablename__ = "project"
 
-    id: Mapped[str] = mapped_column("id", String(255), nullable = False, primary_key = True)
-    name: Mapped[str] = mapped_column("name", String(255), nullable = False)
-    created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable = False, default = datetime.now)
+    id: Mapped[str] = mapped_column("id", String(255), nullable=False, primary_key=True)
+    domain_id: Mapped[str] = mapped_column(ForeignKey("domain.id"), nullable=False)
+    name: Mapped[str] = mapped_column("name", String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable=False, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(
-        "updated_at", DateTime, nullable = False, default=datetime.now, onupdate=datetime.now
+        "updated_at", DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
     )
-    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable = True, default = None)
+    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable=True, default=None)
