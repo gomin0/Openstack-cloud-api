5e9465dad7d20c3016d923d0b0600da0e7213c10
Ted <my.koh@gabia.com>
2025-03-26 14:41:33 +0900

    feat: #3 'User' entity 구현

---
+++ domain/user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[str] = mapped_column("id", String(255), nullable = False, primary_key = True)
+    domain_id: Mapped[str] = mapped_column("id", String(255), nullable = True)
+    name: Mapped[str] = mapped_column("name", String(255), nullable = False)
+    password: Mapped[str] = mapped_column("password", String(255), nullable = False)
+    created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable = False, default = datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at", DateTime, nullable = False, default=datetime.now, onupdate=datetime.now
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable = True, default = None)
