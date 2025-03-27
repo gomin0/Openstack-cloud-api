0a00f9f591ee98a2333888e8cce6c2a54f3094c8
wo_ody <ju.jeong@gabia.com>
2025-03-27 14:58:02 +0900

    feat: #8 `UserResponse` 내의 오픈스택 리소스 id field name 변경

--- router/user/response.py
+++ router/user/response.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel, Field
 
 class UserResponse(BaseModel):
     id: int = Field(description="id", examples=["1"])
-    uuid: str = Field(description="오픈스택 리소스 id", examples=["779b35a7173444e387a7f34134a56e31"])
-    domain_id: int = Field(description="id of domain", examples=["1"])
+    openstack_id: str = Field(description="오픈스택 리소스 id", examples=["779b35a7173444e387a7f34134a56e31"])
+    domain_id: int = Field(description="소속된 도메인의 id", examples=["1"])
     account_id: str = Field(description="로그인 id", examples=["woody0105"])
     name: str = Field(description="이름", examples=["woody"])
     created_at: datetime = Field(description="생성일")
