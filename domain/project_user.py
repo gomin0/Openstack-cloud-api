5fa839300641ccfa48a6a5b6843193449caad0cc
Ted <my.koh@gabia.com>
2025-03-27 10:19:14 +0900

    feat: #3 'ProjectUser' fk type 변경

--- domain/project_user.py
+++ domain/project_user.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 
-from sqlalchemy import String, DateTime, BigInteger, ForeignKey
+from sqlalchemy import String, DateTime, BigInteger, ForeignKey, Enum, CHAR
 from sqlalchemy.orm import Mapped, mapped_column
 
+from domain.status import Status
 from infrastructure.database import Base
 
 
 class ProjectUser(Base):
     __tablename__ = "project_user"
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    user_id: Mapped[str] = mapped_column(ForeignKey("user.id"), nullable=False)
-    project_id: Mapped[str] = mapped_column(ForeignKey("project.id"), nullable=False)
-    role_id: Mapped[str] = mapped_column(ForeignKey("role.id"), nullable=False)
+    id: Mapped[int] = mapped_column("id", BigInteger, primary_key=True)
+    user_id: Mapped[str] = mapped_column("user_id", BigInteger, ForeignKey("user.id"), nullable=False)
+    project_id: Mapped[str] = mapped_column("project_id", BigInteger, ForeignKey("project.id"), nullable=False)
+    role_id: Mapped[str] = mapped_column("role_id", CHAR(32), ForeignKey("role.id"), nullable=False)
     created_at: Mapped[datetime] = mapped_column("created_at", DateTime, nullable=False, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(
         "updated_at", DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
