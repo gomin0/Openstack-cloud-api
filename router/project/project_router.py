63ea3c595af4bfe796c50813a1afa62b582048a1
Ted <my.koh@gabia.com>
2025-03-27 13:32:37 +0900

    feat: #9 프로젝트 변경 API 명세

--- router/project/project_router.py
+++ router/project/project_router.py
@@ -25,3 +25,17 @@ async def get_projects() -> ProjectListResponse:
 )
 async def get_project() -> ProjectResponse:
     raise NotImplementedError()
+
+
+@router.patch(
+    "/{project_id}",
+    response_model=ProjectResponse,
+    summary="프로젝트 변경",
+    responses={
+        400: {"description": "이름이 중복된 경우"},
+        404: {"description": "해당 ID의 프로젝트가 없는 경우"},
+        500: {"description": "OpenStack 연동 실패 등 내부 에러"},
+    }
+)
+async def update_project() -> ProjectResponse:
+    raise NotImplementedError()
