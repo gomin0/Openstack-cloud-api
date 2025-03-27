00cbbd0deda7ea39e4e6c0b19187fececb247056
wo_ody <ju.jeong@gabia.com>
2025-03-27 15:08:00 +0900

    feat: #8 회원 가입 API 명세 작성

---
+++ router/user/request.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+
+
+class SignUpRequest(BaseModel):
+    account_id: str = Field(max_length=20, description="로그인 id", examples=["example1234"])
+    password: str = Field(description="비밀번호", examples=["1q2w3e4r"])
+    name: str = Field(max_length=15, description="이름", examples=["woody"])
--- router/user/user_router.py
+++ router/user/user_router.py
@@ -1,20 +1,37 @@
 from fastapi import APIRouter
 
+from router.user.request import SignUpRequest
 from router.user.response import UserListResponse, UserResponse
 
 router = APIRouter(prefix="/users", tags=["user"])
 
 
-@router.get(path="", status_code=200)
+@router.get(
+    path="", status_code=200,
+    summary="유저 목록 조회"
+)
 def find_users() -> UserListResponse:
     raise NotImplementedError()
 
 
 @router.get(
     path="/{user_id}", status_code=200,
+    summary="유저 단건 조회",
     responses={
         404: {"description": "user_id에 해당하는 유저를 찾을 수 없는 경우"}
     }
 )
 def get_user(user_id: int) -> UserResponse:
+    raise NotImplementedError()
+
+
+@router.post(
+    path="", status_code=201,
+    summary="회원 가입",
+    responses={
+        409: {"description": "로그인 id, 이름이 이미 사용중인 경우"},
+        422: {"description": "요청 데이터의 값이나 형식이 잘못된 경우"}
+    }
+)
+def sign_up(reqeust: SignUpRequest) -> UserResponse:
     raise NotImplementedError()
