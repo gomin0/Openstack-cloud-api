2696bded4e56689278addeed6717185c2d8207bf
wo_ody <ju.jeong@gabia.com>
2025-03-27 17:52:52 +0900

    docs: #8 프로젝트 응답 객체의 필드 설명 오타 수정

--- router/project/response.py
+++ router/project/response.py
@@ -28,9 +28,9 @@ class ProjectResponse(BaseModel):
     id: int = Field(description="프로젝트 id", examples=[1])
     openstack_id: str = Field(description="프로젝트 uuid", examples=["779b35a7173444e387a7f34134a56e31"])
     domain_id: int = Field(description="프로젝트가 속한 도메인 정보")
-    created_at: datetime = Field(description="생성일", examples=["2025-03-027T00:00:00"])
-    updated_at: datetime = Field(description="생성일", examples=["2025-03-027T00:00:00"])
-    deleted_at: datetime | None = Field(None, description="생성일", examples=["2025-03-027T00:00:00"])
+    created_at: datetime = Field(description="생성일")
+    updated_at: datetime = Field(description="수정일")
+    deleted_at: datetime | None = Field(None, description="삭제일")
 
 
 class ProjectDetailResponse(BaseModel):
@@ -38,9 +38,9 @@ class ProjectDetailResponse(BaseModel):
     openstack_id: str = Field(description="프로젝트 uuid", examples=["779b35a7173444e387a7f34134a56e31"])
     domain: DomainResponse = Field(description="프로젝트가 속한 도메인 정보")
     accounts: List[UserResponse] = Field(description="프로젝트에 속한 계정 목록")
-    created_at: datetime = Field(description="생성일", examples=["2025-03-027T00:00:00"])
-    updated_at: datetime = Field(description="생성일", examples=["2025-03-027T00:00:00"])
-    deleted_at: datetime | None = Field(None, description="생성일", examples=["2025-03-027T00:00:00"])
+    created_at: datetime = Field(description="생성일")
+    updated_at: datetime = Field(description="생성일")
+    deleted_at: datetime | None = Field(None, description="생성일")
 
 
 class ProjectListResponse(BaseModel):
