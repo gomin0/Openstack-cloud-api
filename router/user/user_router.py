01f1b5ea1923660914cc911e2311726f66a81da6
wo_ody <ju.jeong@gabia.com>
2025-03-27 13:26:25 +0900

    feat: #8 유저 단건 조회 API 명세 작성

--- router/user/user_router.py
+++ router/user/user_router.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter
 
-from router.user.response import UserListResponse
+from router.user.response import UserListResponse, UserResponse
 
 router = APIRouter(prefix="/users", tags=["user"])
 
 
-@router.get("", status_code=200)
+@router.get(path="", status_code=200)
 def find_users() -> UserListResponse:
+    raise NotImplementedError()
+
+
+@router.get(
+    path="/{user_id}", status_code=200,
+    responses={
+        404: {"description": "user_id에 해당하는 유저를 찾을 수 없는 경우"}
+    }
+)
+def get_user(user_id: int) -> UserResponse:
     raise NotImplementedError()
