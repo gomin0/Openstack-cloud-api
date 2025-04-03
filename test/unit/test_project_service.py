97d59beed4fa667dbb6fd7764bc84e24fe66f3d2
Ted <my.koh@gabia.com>
2025-04-03 11:37:00 +0900

    refactor: #13 fixture 수정

--- test/unit/test_project_service.py
+++ test/unit/test_project_service.py
@@ -6,12 +6,12 @@ from domain.project.enum import ProjectSortOption
 from exception.project_exception import ProjectNotFoundException
 
 
-async def test_find_projects(mock_session, mock_repository, project_service):
+async def test_find_projects(mock_session, mock_project_repository, project_service):
     # given
     name_like = "a"
     project1 = Project(id=1, name="Alpha", domain_id=1, openstack_id="1cd74dcf765544d79a8d5fb7db589133")
     project2 = Project(id=2, name="Beta", domain_id=1, openstack_id="50d30edec2af40fcba9c3e0783bb29ea")
-    mock_repository.find_all.return_value = [project1, project2]
+    mock_project_repository.find_all.return_value = [project1, project2]
 
     # when
     result = await project_service.find_projects(
@@ -24,7 +24,7 @@ async def test_find_projects(mock_session, mock_repository, project_service):
 
     # then
     assert result == [project1, project2]
-    mock_repository.find_all.assert_called_once_with(
+    mock_project_repository.find_all.assert_called_once_with(
         session=mock_session,
         ids=None,
         name=None,
@@ -35,13 +35,13 @@ async def test_find_projects(mock_session, mock_repository, project_service):
     )
 
 
-async def test_get_project(mock_session, mock_repository, project_service):
+async def test_get_project(mock_session, mock_project_repository, project_service):
     # given
     project_id = 1
     domain_id = 1
     project = Project(id=project_id, name="Test", domain_id=domain_id, openstack_id="1cd74dcf765544d79a8d5fb7db589133")
 
-    mock_repository.find_by_id.return_value = project
+    mock_project_repository.find_by_id.return_value = project
 
     # when
     result = await project_service.get_project(
@@ -52,17 +52,17 @@ async def test_get_project(mock_session, mock_repository, project_service):
 
     # then
     assert result == project
-    mock_repository.find_by_id.assert_called_once_with(
+    mock_project_repository.find_by_id.assert_called_once_with(
         session=mock_session,
         project_id=project_id,
         with_relations=True
     )
 
 
-async def test_get_project_fail_not_found(mock_session, mock_repository, project_service):
+async def test_get_project_fail_not_found(mock_session, mock_project_repository, project_service):
     # given
     project_id = 999
-    mock_repository.find_by_id.return_value = None
+    mock_project_repository.find_by_id.return_value = None
 
     # when & then
     with pytest.raises(ProjectNotFoundException):
@@ -72,7 +72,7 @@ async def test_get_project_fail_not_found(mock_session, mock_repository, project
             with_relations=True
         )
 
-    mock_repository.find_by_id.assert_called_once_with(
+    mock_project_repository.find_by_id.assert_called_once_with(
         session=mock_session,
         project_id=project_id,
         with_relations=True
