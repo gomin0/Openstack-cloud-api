6b02668e35b5a94773f023e3104ba48cb5c7eb86
Ted <my.koh@gabia.com>
2025-04-10 14:15:12 +0900

    refactor: #14 exception 메시지 수정

--- exception/common_exception.py
+++ exception/common_exception.py
@@ -15,5 +15,5 @@ class OptimisticLockConflictException(CustomException):
         super().__init__(
             code="OPTIMISTIC_LOCK_CONFLICT",
             status_code=409,
-            message="작업 중 충돌이 발생했습니다."
+            message="다른 사용자가 데이터를 수정하여 요청하신 내용을 처리할 수 없습니다. 잠시 후 다시 시도해주세요."
         )
