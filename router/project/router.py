5c5031e82893e4a0b1a07853b05a3f675849f536
Ted <my.koh@gabia.com>
2025-04-10 11:49:35 +0900

    refactor: #14 삭제 복구

--- router/project/router.py
+++ router/project/router.py
@@ -71,8 +71,8 @@ async def get_project(
     "/{project_id}",
     summary="프로젝트 변경", status_code=200,
     responses={
-        404: {"description": "해당 ID의 프로젝트가 없는 경우"},
         403: {"description": "해당 프로젝트에 대한 접근 권한이 없는 경우"},
+        404: {"description": "해당 ID의 프로젝트가 없는 경우"},
         409: {"description": "이름이 중복된 경우"},
         422: {"description": "요청 데이터의 값이나 형식이 잘못된 경우"}
     }
@@ -96,6 +96,37 @@ async def update_project(
             new_name=request.name
         )
     return ProjectResponse.from_entity(project)
+
+
+@router.delete(
+    "/{project_id}/users/{user_id}",
+    summary="프로젝트에서 계정 제외", status_code=204,
+    responses={
+        404: {"description": "프로젝트, 계정이 없는 경우"},
+        409: {"description": "해당 계정이 이 프로젝트에 소속되어 있지 않은 경우"}
+    }
+)
+async def remove_account_from_project(
+    project_id: int = Path(description="프로젝트 ID"),
+    user_id: int = Path(description="계정 ID")
+) -> None:
+    raise NotImplementedError()
+
+
+@router.post(
+    "/{project_id}/users/{user_id}",
+    summary="프로젝트에 계정 소속", status_code=204,
+    responses={
+        404: {"description": "프로젝트, 계정이 없는 경우"},
+        409: {"description": "이미 소속된 경우"}
+    }
+)
+async def assign_account_to_project(
+    project_id: int = Path(description="프로젝트 ID"),
+    user_id: int = Path(description="계정 ID")
+) -> None:
+    raise NotImplementedError()
+
 
 @router.delete(
     "/{project_id}/users/{user_id}",
