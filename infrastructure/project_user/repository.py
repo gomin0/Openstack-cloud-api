ae14335e0cbfa6cb7c479cc2c7c7fe0a5c6c9c67
Ted <my.koh@gabia.com>
2025-04-14 10:13:56 +0900

    feat: #15 프로젝트에 유저 소속 repository 작성

---
+++ infrastructure/project_user/repository.py
@@ -0,0 +1,45 @@
+from sqlalchemy import select, exists
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from domain.project.entity import ProjectUser
+
+
+class ProjectUserRepository:
+    async def exists_by_user_and_project(
+        self,
+        session: AsyncSession,
+        project_id: int,
+        user_id: int
+    ) -> bool:
+        stmt = select(exists().where(
+            ProjectUser.project_id == project_id,
+            ProjectUser.user_id == user_id
+        ))
+        result = await session.scalar(stmt)
+        return result
+
+    async def is_user_role_exist(
+        self,
+        session: AsyncSession,
+        project_id: int,
+        user_id: int,
+        role_id: str
+    ) -> bool:
+        result: bool = await session.scalar(
+            select(
+                exists().where(
+                    ProjectUser.project_id == project_id,
+                    ProjectUser.user_id == user_id,
+                    ProjectUser.role_id == role_id
+                )
+            )
+        )
+        return result
+
+    async def add_user_role(
+        self,
+        session: AsyncSession,
+        project_user: ProjectUser,
+    ) -> None:
+        session.add(project_user)
+        await session.flush()
