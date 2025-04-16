014b7d9f0562d0b69ae2a5fe2f4b1bdc924b6fd5
Ted <my.koh@gabia.com>
2025-04-16 14:27:35 +0900

    feat: #54 보안그룹 단일 조회 api 명세 작성

--- router/security_group/router.py
+++ router/security_group/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query
 
 from domain.enum import SortOrder
-from router.security_group.response import SecurityGroupDetailResponses
+from router.security_group.response import SecurityGroupDetailResponses, SecurityGroupDetailResponse
 
 router = APIRouter(prefix="/security-group", tags=["security-group"])
 
@@ -17,4 +17,18 @@ async def find_security_groups(
     sort_by: SecurityGroupSortOption = Query(default=SecurityGroupSortOption.CREATED_AT),
     order: SortOrder = Query(default=SortOrder.ASC),
 ) -> SecurityGroupDetailResponses:
+    raise NotImplementedError()
+
+
+@router.get(
+    "/{security_group_id}",
+    status_code=200,
+    summary="보안그룹 단일 조회",
+    responses={
+        404: {"description": "해당 ID의 보안그룹을 찾을 수 없는 경우"}
+    }
+)
+async def get_security_group(
+    security_group_id: int,
+) -> SecurityGroupDetailResponse:
     raise NotImplementedError()
