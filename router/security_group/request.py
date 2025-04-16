71b20fb4b90394e65cc1e97f3e085cd8b96715a4
Ted <my.koh@gabia.com>
2025-04-16 15:07:55 +0900

    feat: #54 default value 수정

--- router/security_group/request.py
+++ router/security_group/request.py
@@ -4,10 +4,10 @@ from domain.security_group.enum import SecurityGroupRuleDirection
 
 
 class SecurityGroupRuleRequest(BaseModel):
-    protocol: str | None = Field(description="프로토콜", examples=["tcp"])
+    protocol: str | None = Field(default=None, description="프로토콜", examples=["tcp"])
     direction: SecurityGroupRuleDirection = Field(description="방향", examples=["ingress"])
-    port_range_min: int = Field(default=None, description="시작 포트", examples=[22])
-    port_range_max: int = Field(default=None, description="종료 포트", examples=[22])
+    port_range_min: int = Field(description="시작 포트", examples=[22])
+    port_range_max: int = Field(description="종료 포트", examples=[22])
     remote_ip_prefix: str | None = Field(default=None, description="CIDR", examples=["0.0.0.0/0"])
 
 
--- router/security_group/response.py
+++ router/security_group/response.py
@@ -7,10 +7,10 @@ from domain.security_group.enum import SecurityGroupRuleDirection
 
 class SecurityGroupRuleResponse(BaseModel):
     id: int = Field(description="룰셋 ID", examples=[1])
-    protocol: str | None = Field(description="프로토콜", examples=["tcp"])
+    protocol: str | None = Field(default=None, description="프로토콜", examples=["tcp"])
     direction: SecurityGroupRuleDirection = Field(description="방향", examples=["ingress"])
-    port_range_min: int = Field(default=None, description="시작 포트", examples=[22])
-    port_range_max: int = Field(default=None, description="종료 포트", examples=[22])
+    port_range_min: int = Field(description="시작 포트", examples=[22])
+    port_range_max: int = Field(description="종료 포트", examples=[22])
     remote_ip_prefix: str | None = Field(default=None, description="CIDR", examples=["0.0.0.0/0"])
     created_at: datetime = Field(description="생성일")
     updated_at: datetime = Field(description="수정일")
