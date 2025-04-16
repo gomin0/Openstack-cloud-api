40fa0fdea9273b2bf682e817457198aac46312c7
Ted <my.koh@gabia.com>
2025-04-16 15:39:41 +0900

    feat: #57 request 수정

--- router/floating_ip/request.py
+++ router/floating_ip/request.py
@@ -3,4 +3,3 @@ from pydantic import BaseModel, Field
 
 class CreateFloatingIPRequest(BaseModel):
     project_id: int = Field(description="플로팅 IP를 할당할 프로젝트 ID", examples=[1])
-    public_network_id: str = Field(description="플로팅 IP를 할당받을 공개 네트워크 ID", examples=["123"])
