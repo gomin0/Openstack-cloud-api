bb34e0763dd665ae5066e0cd7bdc6797443c11fe
Ted <my.koh@gabia.com>
2025-04-18 14:23:07 +0900

    feat: #54 접근 권한 추가

--- router/security_group/router.py
+++ router/security_group/router.py
@@ -43,6 +43,7 @@ async def get_security_group(
     summary="보안그룹 생성",
     responses={
         401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "보안 그룹 생성 권한이 없는 경우"},
         409: {"description": "보안그룹 이름이 이미 프로젝트 내에서 사용중인 경우"},
         422: {"description": "요청 데이터의 값이나 형식이 잘못된 경우"}
     }
@@ -59,6 +60,7 @@ async def create_security_group(
     summary="보안그룹 변경",
     responses={
         401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "보안 그룹 변경 권한이 없는 경우"},
         404: {"description": "해당 ID의 보안그룹을 찾을 수 없는 경우"},
         409: {"description": "변경하려는 이름이 이미 사용중인 경우"},
         422: {"description": "요청 데이터의 값이나 형식이 잘못된 경우"}
@@ -77,6 +79,7 @@ async def update_security_group(
     summary="보안그룹 삭제",
     responses={
         401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "보안 그룹 삭제 권한이 없는 경우"},
         404: {"description": "해당 ID의 보안그룹을 찾을 수 없는 경우"},
         409: {"description": "연결된 서버가 존재해 삭제할 수 없는 경우"}
     }
