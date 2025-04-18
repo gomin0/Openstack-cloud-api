769bf5db44ea702e59c76709661fbb8eef8b04d7
Ted <my.koh@gabia.com>
2025-04-18 17:31:57 +0900

    refactor: #50 server_id null=True

--- domain/network_interface/entity.py
+++ domain/network_interface/entity.py
@@ -11,7 +11,7 @@ class NetworkInterface(Base):
     __tablename__ = "network_interface"
 
     id: Mapped[int] = mapped_column("id", BigInteger, primary_key=True, autoincrement=True)
-    server_id: Mapped[int | None] = mapped_column("server_id", BigInteger, ForeignKey("server.id"), nullable=False)
+    server_id: Mapped[int | None] = mapped_column("server_id", BigInteger, ForeignKey("server.id"), nullable=True)
     port_openstack_id: Mapped[str] = mapped_column("openstack_id", CHAR(36), nullable=False)
     fixed_ip_address: Mapped[str] = mapped_column("fixed_ip_address", String(15), nullable=False)
     lifecycle_status: Mapped[LifecycleStatus] = mapped_column(
