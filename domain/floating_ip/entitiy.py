49cc781e9108dd9abe1fa4ce18f04f2eb49de744
Ted <my.koh@gabia.com>
2025-04-18 11:40:31 +0900

    feat: #50 floating ip entity 설계

---
+++ domain/floating_ip/entitiy.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import BigInteger, CHAR, ForeignKey, String, DateTime, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from domain.entity import Base
+from domain.enum import LifecycleStatus
+from domain.floating_ip.enum import FloatingIpStatus
+
+
+class FloatingIp(Base):
+    __tablename__ = "floating_ip"
+
+    id: Mapped[int] = mapped_column("id", BigInteger, primary_key=True, autoincrement=True)
+    openstack_id: Mapped[str] = mapped_column("openstack_id", CHAR(36), nullable=False)
+    project_id: Mapped[int] = mapped_column("project_id", BigInteger, ForeignKey("project.id"), nullable=False)
+    server_id: Mapped[int | None] = mapped_column("server_id", BigInteger, ForeignKey("server.id"), nullable=True)
+    status: Mapped[FloatingIpStatus] = mapped_column(
+        Enum(FloatingIpStatus, name="status", native_enum=False, length=30),
+        nullable=False
+    )
+    address: Mapped[str] = mapped_column("address", String(15), nullable=False)
+    lifecycle_status: Mapped[LifecycleStatus] = mapped_column(
+        Enum(LifecycleStatus, name="sg_lifecycle_status", native_enum=False, length=15),
+        nullable=False,
+        default=LifecycleStatus.ACTIVE
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at", DateTime, nullable=False, default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at", DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc)
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column("deleted_at", DateTime, nullable=True)
