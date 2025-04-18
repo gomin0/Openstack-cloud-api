a175b0de062a4983d1b407c6c7d11b8948f1f189
Ted <my.koh@gabia.com>
2025-04-18 16:04:58 +0900

    refactor: #54 권한 수정 및 CurrentUser 명시

--- router/security_group/router.py
+++ router/security_group/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Depends
 
+from common.auth_token_manager import get_current_user
+from common.context import CurrentUser
 from domain.enum import SortOrder
 from domain.security_group.enum import SecurityGroupSortOption
 from router.security_group.request import CreateSecurityGroupRequest, UpdateSecurityGroupRequest
@@ -19,6 +21,7 @@ router = APIRouter(prefix="/security-groups", tags=["security-group"])
 async def find_security_groups(
     sort_by: SecurityGroupSortOption = Query(default=SecurityGroupSortOption.CREATED_AT),
     order: SortOrder = Query(default=SortOrder.ASC),
+    _: CurrentUser = Depends(get_current_user),
 ) -> SecurityGroupDetailsResponse:
     raise NotImplementedError()
 
@@ -29,11 +32,13 @@ async def find_security_groups(
     summary="보안그룹 단일 조회",
     responses={
         401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "보안 그룹 접근 권한이 없는 경우"},
         404: {"description": "해당 ID의 보안그룹을 찾을 수 없는 경우"}
     }
 )
 async def get_security_group(
     security_group_id: int,
+    _: CurrentUser = Depends(get_current_user),
 ) -> SecurityGroupDetailResponse:
     raise NotImplementedError()
 
@@ -43,13 +48,13 @@ async def get_security_group(
     summary="보안그룹 생성",
     responses={
         401: {"description": "인증 정보가 유효하지 않은 경우"},
-        403: {"description": "보안 그룹 생성 권한이 없는 경우"},
         409: {"description": "보안그룹 이름이 이미 프로젝트 내에서 사용중인 경우"},
         422: {"description": "요청 데이터의 값이나 형식이 잘못된 경우"}
     }
 )
 async def create_security_group(
     request: CreateSecurityGroupRequest,
+    _: CurrentUser = Depends(get_current_user),
 ) -> SecurityGroupDetailResponse:
     raise NotImplementedError()
 
@@ -69,6 +74,7 @@ async def create_security_group(
 async def update_security_group(
     security_group_id: int,
     request: UpdateSecurityGroupRequest,
+    _: CurrentUser = Depends(get_current_user),
 ) -> SecurityGroupDetailResponse:
     raise NotImplementedError()
 
@@ -86,5 +92,6 @@ async def update_security_group(
 )
 async def delete_security_group(
     security_group_id: int,
+    _: CurrentUser = Depends(get_current_user),
 ) -> None:
     raise NotImplementedError()
