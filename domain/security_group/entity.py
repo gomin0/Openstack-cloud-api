ae186b0d19951aa372b002f49aede0ba73a08e2d
Ted <my.koh@gabia.com>
2025-04-21 09:49:09 +0900

    feat: #50 보안그룹 description nullable

--- domain/security_group/entity.py
+++ domain/security_group/entity.py
@@ -14,7 +14,7 @@ class SecurityGroup(Base):
     openstack_id: Mapped[str] = mapped_column("openstack_id", CHAR(36), nullable=False)
     project_id: Mapped[int] = mapped_column("project_id", BigInteger, ForeignKey("project.id"), nullable=False)
     name: Mapped[str] = mapped_column("name", String(255), nullable=False)
-    description: Mapped[str] = mapped_column("description", String(255), nullable=False)
+    description: Mapped[str] = mapped_column("description", String(255), nullable=True)
     lifecycle_status: Mapped[LifecycleStatus] = mapped_column(
         Enum(LifecycleStatus, name="lifecycle_status", native_enum=False, length=15),
         nullable=False,
