8a72a8d8c6e384ac2608cc6a6255080ade3f90df
Ted <my.koh@gabia.com>
2025-04-23 09:54:30 +0900

    feat: #26 보안그룹 목록 조회 client 작성

---
+++ infrastructure/neutron/client.py
@@ -0,0 +1,26 @@
+from httpx import AsyncClient, Response
+
+from common.envs import get_envs
+from infrastructure.openstack_client import OpenStackClient
+
+envs = get_envs()
+
+
+class NeutronClient(OpenStackClient):
+    _OPEN_STACK_URL: str = envs.OPENSTACK_SERVER_URL
+    _NEUTRON_PORT: int = envs.NEUTRON_PORT
+    _NEUTRON_URL: str = f"{_OPEN_STACK_URL}:{_NEUTRON_PORT}"
+
+    async def get_security_group_rules_in_project(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        project_openstack_id: str,
+    ) -> list[dict]:
+        response: Response = await self.request(
+            client=client,
+            method="GET",
+            url=f"{self._NEUTRON_URL}/v2.0/security-group-rules?project_id={project_openstack_id}",
+            headers={"X-Auth-Token": keystone_token}
+        )
+        return response.json().get("security_group_rules", [])
