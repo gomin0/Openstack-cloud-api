811fc2752aa8d4ec9a9b4613be4f74d4e30e0ee9
Ted <my.koh@gabia.com>
2025-04-23 10:55:52 +0900

    feat: #29 보안그룹 생성 request 수정

--- router/security_group/request.py
+++ router/security_group/request.py
@@ -5,7 +5,7 @@ from domain.security_group.enum import SecurityGroupRuleDirection
 
 class SecurityGroupRuleRequest(BaseModel):
     protocol: str | None = Field(max_length=10, default=None, description="프로토콜", examples=["tcp"])
-    direction: SecurityGroupRuleDirection = Field(max_length=10, description="방향", examples=["ingress"])
+    direction: SecurityGroupRuleDirection = Field(description="방향", examples=["ingress"])
     port_range_min: int = Field(description="시작 포트", examples=[22])
     port_range_max: int = Field(description="종료 포트", examples=[22])
     remote_ip_prefix: str = Field(max_length=43, description="CIDR", examples=["0.0.0.0/0"])
