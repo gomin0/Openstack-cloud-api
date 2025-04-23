3764546df8d7f6267f2d24277888ac0828918d88
Ted <my.koh@gabia.com>
2025-04-23 11:09:17 +0900

    feat: #29 보안그룹 생성 service 작성

--- application/security_group/service.py
+++ application/security_group/service.py
@@ -2,11 +2,15 @@ from fastapi import Depends
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from application.security_group.dto import SecurityGroupRuleDTO
 from application.security_group.response import SecurityGroupDetailsResponse, SecurityGroupDetailResponse
+from common.compensating_transaction import CompensationManager
 from domain.enum import SortOrder
 from domain.security_group.entity import SecurityGroup
 from domain.security_group.enum import SecurityGroupSortOption
-from exception.security_group_exception import SecurityGroupNotFoundException, SecurityGroupAccessDeniedException
+from exception.openstack_exception import OpenStackException
+from exception.security_group_exception import SecurityGroupAccessDeniedException, SecurityGroupRuleDuplicatedException
+from exception.security_group_exception import SecurityGroupNotFoundException, SecurityGroupNameDuplicatedException
 from infrastructure.database import transactional
 from infrastructure.neutron.client import NeutronClient
 from infrastructure.security_group.repository import SecurityGroupRepository
@@ -91,3 +95,89 @@ class SecurityGroupService:
         )
 
         return await SecurityGroupDetailResponse.from_entity(security_group, rules)
+
+    @transactional()
+    async def create_security_group(
+        self,
+        compensating_tx: CompensationManager,
+        session: AsyncSession,
+        client: AsyncClient,
+        keystone_token: str,
+        project_id: int,
+        name: str,
+        description: str | None,
+        rules: list[SecurityGroupRuleDTO]
+    ) -> SecurityGroupDetailResponse:
+        if await self.security_group_repository.exists_by_project_and_name(
+            session=session,
+            project_id=project_id,
+            name=name
+        ):
+            raise SecurityGroupNameDuplicatedException()
+
+        try:
+            openstack_security_group: dict = await self.neutron_client.create_security_group(
+                client=client,
+                keystone_token=keystone_token,
+                name=name,
+                description=description
+            )
+
+            security_group_openstack_id: str = openstack_security_group["id"]
+
+            compensating_tx.add_task(
+                lambda: self.neutron_client.delete_security_group(
+                    client=client,
+                    keystone_token=keystone_token,
+                    security_group_openstack_id=security_group_openstack_id
+                )
+            )
+        except OpenStackException as ex:
+            if ex.openstack_status_code == 409:
+                raise SecurityGroupNameDuplicatedException()
+            raise ex
+
+        security_group: SecurityGroup = await self.security_group_repository.create(
+            session=session,
+            security_group=SecurityGroup(
+                openstack_id=security_group_openstack_id,
+                project_id=project_id,
+                name=name,
+                description=description,
+            )
+        )
+
+        if rules:
+            try:
+                security_group_rules = [
+                    {
+                        "direction": rule.direction.value,
+                        "protocol": rule.protocol,
+                        "port_range_min": rule.port_range_min,
+                        "port_range_max": rule.port_range_max,
+                        "remote_ip_prefix": rule.remote_ip_prefix,
+                        "security_group_id": security_group_openstack_id,
+                    }
+                    for rule in rules
+                ]
+
+                await self.neutron_client.create_security_group_rules(
+                    client=client,
+                    keystone_token=keystone_token,
+                    security_group_rules=security_group_rules
+                )
+            except OpenStackException as ex:
+                if ex.openstack_status_code == 404:
+                    raise SecurityGroupNotFoundException()
+                if ex.openstack_status_code == 409:
+                    raise SecurityGroupRuleDuplicatedException()
+                raise ex
+
+        # 생성 된 룰 조회(생성한 룰셋 + default rule)
+        security_group_rules = await self.neutron_client.get_security_group_rules_in_security_group(
+            client=client,
+            keystone_token=keystone_token,
+            security_group_openstack_id=security_group_openstack_id
+        )
+
+        return await SecurityGroupDetailResponse.from_entity(security_group, security_group_rules)
