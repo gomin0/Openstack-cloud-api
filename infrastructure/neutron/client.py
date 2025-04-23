e7b7e7e080f50b87040b8518da523deab19a18b2
Ted <my.koh@gabia.com>
2025-04-23 10:54:41 +0900

    feat: #29 보안그룹 생성 client 작성

--- infrastructure/neutron/client.py
+++ infrastructure/neutron/client.py
@@ -31,10 +31,58 @@ class NeutronClient(OpenStackClient):
         keystone_token: str,
         security_group_openstack_id: str
     ) -> list[dict]:
-        response = await self.request(
+        response: Response = await self.request(
             client=client,
             method="GET",
             url=f"{self._NEUTRON_URL}/v2.0/security-group-rules?security_group_id={security_group_openstack_id}",
             headers={"X-Auth-Token": keystone_token}
         )
         return response.json().get("security_group_rules", [])
+
+    async def create_security_group(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        name: str,
+        description: str
+    ) -> dict:
+        response: Response = await self.request(
+            client=client,
+            method="POST",
+            url=f"{self._NEUTRON_URL}/v2.0/security-groups",
+            headers={"X-Auth-Token": keystone_token},
+            json={
+                "security_group": {
+                    "name": name,
+                    "description": description
+                }
+            }
+        )
+        return response.json()["security_group"]
+
+    async def delete_security_group(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        security_group_openstack_id: str
+    ) -> None:
+        await self.request(
+            client=client,
+            method="DELETE",
+            url=f"{self._NEUTRON_URL}/v2.0/security-groups/{security_group_openstack_id}",
+            headers={"X-Auth-Token": keystone_token}
+        )
+
+    async def create_security_group_rules(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        security_group_rules: list[dict]
+    ) -> None:
+        await self.request(
+            client=client,
+            method="POST",
+            url=f"{self._NEUTRON_URL}/v2.0/security-group-rules",
+            headers={"X-Auth-Token": keystone_token},
+            json={"security_group_rules": security_group_rules}
+        )
