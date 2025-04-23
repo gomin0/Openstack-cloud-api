931af6911127ed374cb321d748b94aa784ce4566
Ted <my.koh@gabia.com>
2025-04-23 11:09:09 +0900

    feat: #29 보안그룹 생성 exception 작성

--- exception/security_group_exception.py
+++ exception/security_group_exception.py
@@ -17,3 +17,21 @@ class SecurityGroupAccessDeniedException(CustomException):
             status_code=403,
             message="해당 보안 그룹에 접근할 수 권한이 없습니다."
         )
+
+
+class SecurityGroupNameDuplicatedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SECURITY_GROUP_NAME_DUPLICATED",
+            status_code=409,
+            message="이미 프로젝트 내에 존재하는 보안 그룹 이름입니다."
+        )
+
+
+class SecurityGroupRuleDuplicatedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SECURITY_GROUP_RULE_DUPLICATED",
+            status_code=409,
+            message="이미 존재하는 보안 그룹 룰 입니다."
+        )
