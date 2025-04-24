bc15e2a6946207c05ea61d1d855c9d7688d246b9
Ted <my.koh@gabia.com>
2025-04-24 18:00:57 +0900

    feat: #37 floating ip 할당(생성) client 작성

---
+++ infrastructure/neutron/client.py
@@ -0,0 +1,42 @@
+from httpx import AsyncClient, Response
+
+from common.envs import get_envs
+from infrastructure.openstack_client import OpenStackClient
+
+envs = get_envs()
+
+
+class NeutronClient(OpenStackClient):
+    _OPEN_STACK_URL: str = envs.OPENSTACK_SERVER_URL
+    _NEUTRON_PORT: int = envs.NEUTRON_PORT
+    _NEUTRON_URL: str = f"{_OPEN_STACK_URL}:{_NEUTRON_PORT}"
+
+    async def create_floating_ip(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        floating_network_id: str,
+    ) -> tuple[str, str]:
+        response: Response = await self.request(
+            client=client,
+            method="POST",
+            url=f"{self._NEUTRON_URL}/v2.0/floatingips",
+            headers={"X-Auth-Token": keystone_token},
+            json={"floatingip": {"floating_network_id": floating_network_id}}
+        )
+        data = response.json()["floatingip"]
+
+        return data["id"], data["floating_ip_address"]
+
+    async def delete_floating_ip(
+        self,
+        client: AsyncClient,
+        keystone_token: str,
+        floating_ip_openstack_id: str,
+    ) -> None:
+        await self.request(
+            client=client,
+            method="DELETE",
+            url=f"{self._NEUTRON_URL}/v2.0/floatingips/{floating_ip_openstack_id}",
+            headers={"X-Auth-Token": keystone_token},
+        )
