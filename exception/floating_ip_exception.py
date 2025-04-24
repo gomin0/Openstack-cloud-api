149125d7042d2bde761e1d5c1c25d120e5f03b6b
Ted <my.koh@gabia.com>
2025-04-24 18:01:04 +0900

    feat: #37 floating ip 할당(생성) exception 작성

---
+++ exception/floating_ip_exception.py
@@ -0,0 +1,10 @@
+from exception.base_exception import CustomException
+
+
+class FloatingNetworkNotFound(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="FLOATING_NETWORK_NOT_FOUND",
+            status_code=404,
+            message="floating ip를 할당 받을 network를 찾을 수 없습니다."
+        )
