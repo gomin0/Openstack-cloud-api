fd9f0b014bb9b8e87f8ce2f235d3d40682690627
Ted <my.koh@gabia.com>
2025-04-28 15:26:40 +0900

    refactor: #37 오타 수정 및 검증 제거

--- common/application/floating_ip/service.py
+++ common/application/floating_ip/service.py
@@ -5,10 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from common.application.floating_ip.response import FloatingIpResponse
 from common.domain.floating_ip.entity import FloatingIp
 from common.domain.floating_ip.enum import FloatingIpStatus
-from common.exception.floating_ip_exception import FloatingNetworkNotFound
-from common.exception.openstack_exception import OpenStackException
 from common.infrastructure.database import transactional
-from common.infrastructure.floating_ip import FloatingIpRepository
+from common.infrastructure.floating_ip.repository import FloatingIpRepository
 from common.infrastructure.neutron.client import NeutronClient
 from common.util.compensating_transaction import CompensationManager
 
@@ -32,29 +30,24 @@ class FloatingIpService:
         keystone_token: str,
         floating_network_id: str,
     ) -> FloatingIpResponse:
-        try:
-            floating_ip_info: tuple[str, str] = await self.neutron_client.create_floating_ip(
+        floating_ip_info: tuple[str, str] = await self.neutron_client.create_floating_ip(
+            client=client,
+            floating_network_id=floating_network_id,
+            keystone_token=keystone_token,
+        )
+        floating_ip_openstack_id: str = floating_ip_info[0]
+        floating_ip_address: str = floating_ip_info[1]
+
+        compensating_tx.add_task(
+            lambda: self.neutron_client.delete_floating_ip(
                 client=client,
-                floating_network_id=floating_network_id,
+                floating_ip_openstack_id=floating_ip_openstack_id,
                 keystone_token=keystone_token,
             )
-            floating_id_openstack_id: str = floating_ip_info[0]
-            floating_ip_address: str = floating_ip_info[1]
-
-            compensating_tx.add_task(
-                lambda: self.neutron_client.delete_floating_ip(
-                    client=client,
-                    floating_ip_openstack_id=floating_id_openstack_id,
-                    keystone_token=keystone_token,
-                )
-            )
-        except OpenStackException as ex:
-            if ex.openstack_status_code == 404:
-                raise FloatingNetworkNotFound() from ex
-            raise ex
+        )
 
         floating_ip: FloatingIp = FloatingIp.create(
-            openstack_id=floating_id_openstack_id,
+            openstack_id=floating_ip_openstack_id,
             project_id=project_id,
             server_id=None,
             status=FloatingIpStatus.DOWN,
--- test/unit/test_floating_ip_service.py
+++ test/unit/test_floating_ip_service.py
@@ -1,7 +1,3 @@
-import pytest
-
-from common.exception.floating_ip_exception import FloatingNetworkNotFound
-from common.exception.openstack_exception import OpenStackException
 from test.util.factory import create_floating_ip_stub
 
 
@@ -49,38 +45,3 @@ async def test_create_floating_ip_success(
         keystone_token=keystone_token
     )
     mock_floating_ip_repository.create.assert_called_once()
-
-
-async def test_create_floating_ip_fail_network_not_found(
-    mock_session,
-    mock_async_client,
-    mock_floating_ip_repository,
-    floating_ip_service,
-    mock_compensation_manager
-):
-    # given
-    floating_network_id = "network-id"
-    keystone_token = "token"
-    project_id = 1
-
-    floating_ip_service.neutron_client.create_floating_ip.side_effect = OpenStackException(
-        openstack_status_code=404,
-    )
-
-    # when & then
-    with pytest.raises(FloatingNetworkNotFound):
-        await floating_ip_service.create_floating_ip(
-            compensating_tx=mock_compensation_manager,
-            session=mock_session,
-            client=mock_async_client,
-            project_id=project_id,
-            keystone_token=keystone_token,
-            floating_network_id=floating_network_id
-        )
-
-    floating_ip_service.neutron_client.create_floating_ip.assert_called_once_with(
-        client=mock_async_client,
-        floating_network_id=floating_network_id,
-        keystone_token=keystone_token
-    )
-    mock_floating_ip_repository.create.assert_not_called()
