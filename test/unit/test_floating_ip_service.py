3c8b530b0dd4dc683a7d1136471026548d97cc60
Ted <my.koh@gabia.com>
2025-04-28 09:57:30 +0900

    test: #39 floating ip 단일 조회 E2E & Unit Test 작성

--- test/unit/test_floating_ip_service.py
+++ test/unit/test_floating_ip_service.py
@@ -1,10 +1,13 @@
+import pytest
+
 from application.floating_ip.response import FloatingIpDetailResponse
 from domain.enum import SortOrder
 from domain.floating_ip.enum import FloatingIpSortOption
+from exception.floating_ip_exception import FloatingIpAccessDeniedException, FloatingIpNotFoundException
 from test.util.factory import create_floating_ip_stub
 
 
-async def test_find_floating_ips(mock_session, mock_floating_ip_repository, floating_ip_service):
+async def test_find_floating_ips_success(mock_session, mock_floating_ip_repository, floating_ip_service):
     # given
     project_id = 1
 
@@ -35,6 +38,72 @@ async def test_find_floating_ips(mock_session, mock_floating_ip_repository, floa
         project_id=project_id,
         sort_by=FloatingIpSortOption.CREATED_AT,
         order=SortOrder.ASC,
+        with_deleted=False,
+        with_relations=True
+    )
+
+
+async def test_get_floating_ip_success(mock_session, mock_floating_ip_repository, floating_ip_service):
+    # given
+    project_id = 1
+    floating_ip = create_floating_ip_stub(project_id=project_id)
+
+    mock_floating_ip_repository.find_by_id.return_value = floating_ip
+
+    # when
+    result = await floating_ip_service.get_floating_ip_detail(
+        session=mock_session,
+        project_id=project_id,
+        floating_ip_id=floating_ip.id
+    )
+
+    # then
+    assert result.id == floating_ip.id
+
+    mock_floating_ip_repository.find_by_id.assert_called_once_with(
+        session=mock_session,
+        floating_ip_id=floating_ip.id,
+        with_deleted=False,
+        with_relations=True
+    )
+
+
+async def test_get_floating_ip_fail_not_found(mock_session, mock_floating_ip_repository, floating_ip_service):
+    # given
+    mock_floating_ip_repository.find_by_id.return_value = None
+
+    # when & then
+    with pytest.raises(FloatingIpNotFoundException):
+        await floating_ip_service.get_floating_ip_detail(
+            session=mock_session,
+            project_id=1,
+            floating_ip_id=1
+        )
+
+    mock_floating_ip_repository.find_by_id.assert_called_once_with(
+        session=mock_session,
+        floating_ip_id=1,
+        with_deleted=False,
+        with_relations=True
+    )
+
+
+async def test_get_floating_ip_fail_access_denied(mock_session, mock_floating_ip_repository, floating_ip_service):
+    # given
+    floating_ip = create_floating_ip_stub(project_id=1)
+    mock_floating_ip_repository.find_by_id.return_value = floating_ip
+
+    # when & then
+    with pytest.raises(FloatingIpAccessDeniedException):
+        await floating_ip_service.get_floating_ip_detail(
+            session=mock_session,
+            project_id=2,
+            floating_ip_id=floating_ip.id
+        )
+
+    mock_floating_ip_repository.find_by_id.assert_called_once_with(
+        session=mock_session,
+        floating_ip_id=floating_ip.id,
         with_deleted=False,
         with_relations=True
     )
