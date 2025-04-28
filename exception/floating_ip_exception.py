d7d8bfe7a2cdbbad2b58153b3bfb567c0d9a5dd4
Ted <my.koh@gabia.com>
2025-04-28 09:57:15 +0900

    feat: #39 floating ip 단일 조회 exception 작성

---
+++ exception/floating_ip_exception.py
@@ -0,0 +1,19 @@
+from exception.base_exception import CustomException
+
+
+class FloatingIpNotFoundException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="FLOATING_IP_NOT_FOUND",
+            status_code=404,
+            message="floating ip를 찾을 수 없습니다."
+        )
+
+
+class FloatingIpAccessDeniedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="FLOATING_IP_ACCESS_DENIED",
+            status_code=403,
+            message="해당 floating ip에 접근할 수 있는 권한이 없습니다."
+        )
