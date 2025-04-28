9fecb52ee0ad40e6debf3881a103df885671c34a
wo_ody <ju.jeong@gabia.com>
2025-04-28 18:32:59 +0900

    style: #34 불필요한 import문 제거

--- api_server/main.py
+++ api_server/main.py
@@ -13,11 +13,8 @@ from api_server.router.project.router import router as project_router
 from api_server.router.security_group.router import router as security_group_router
 from api_server.router.user.router import router as user_router
 from api_server.router.volume.router import router as volume_router
-from common.domain.server.entity import Server
 from common.exception.base_exception import CustomException
 from common.infrastructure.async_client import (init_async_client, close_async_client)
-
-Server  # TODO: 추후 삭제 필요
 
 
 @asynccontextmanager
