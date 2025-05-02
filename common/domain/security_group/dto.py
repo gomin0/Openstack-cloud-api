153f5ba86d3d27e1d5d9cb6c7112c26460f5f38d
Ted <my.koh@gabia.com>
2025-05-02 14:34:16 +0900

    refactor: #30 보안 그룹 변경 dto 수정

--- common/domain/security_group/dto.py
+++ common/domain/security_group/dto.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from pydantic.dataclasses import dataclass
 
 from common.domain.security_group.enum import SecurityGroupRuleDirection
@@ -14,12 +12,19 @@ class SecurityGroupRuleDTO:
     port_range_min: int | None
     port_range_max: int | None
     remote_ip_prefix: str | None
-    created_at: datetime
-    updated_at: datetime
 
 
 @dataclass
 class CreateSecurityGroupRuleDTO:
+    protocol: str | None
+    direction: SecurityGroupRuleDirection
+    port_range_min: int | None
+    port_range_max: int | None
+    remote_ip_prefix: str | None
+
+
+@dataclass
+class UpdateSecurityGroupRuleDTO:
     protocol: str | None
     direction: SecurityGroupRuleDirection
     port_range_min: int | None
