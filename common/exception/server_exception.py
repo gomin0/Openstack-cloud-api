c747f7e548aee168fd343c1b63329a31f9cdc794
Ted <my.koh@gabia.com>
2025-05-08 14:03:27 +0900

    feat: #40 서버 조회 exception 구현

---
+++ common/exception/server_exception.py
@@ -0,0 +1,19 @@
+from common.exception.base_exception import CustomException
+
+
+class ServerNotFoundException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SERVER_NOT_FOUND",
+            status_code=404,
+            message="서버를 찾을 수 없습니다."
+        )
+
+
+class ServerAccessDeniedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SERVER_ACCESS_DENIED",
+            status_code=403,
+            message="해당 서버에 접근할 수 있는 권한이 없습니다."
+        )
