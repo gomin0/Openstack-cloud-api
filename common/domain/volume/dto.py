7b38b094a4fcfa0d7f8008034ccec797bbc45ed4
woody <ju.jeong@gabia.com>
2025-05-08 16:09:10 +0900

    fix: #33 잘못 작성된 type annotation 수정

--- common/domain/volume/dto.py
+++ common/domain/volume/dto.py
@@ -5,7 +5,7 @@ from common.domain.volume.enum import VolumeStatus
 
 @dataclass
 class VolumeDto:
-    openstack_id: int
+    openstack_id: str
     volume_type_name: str
     image_openstack_id: str | None
     status: VolumeStatus
