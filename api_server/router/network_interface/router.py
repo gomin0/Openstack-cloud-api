d5b185ec71d2d479f25f3d18ab078f1258b77b00
Ted <my.koh@gabia.com>
2025-05-13 09:58:11 +0900

    feat: #47 floating ip 연결 / 연결 해제 API router 구현

---
+++ api_server/router/network_interface/router.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, Depends
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_200_OK
+
+from common.application.network_interface.service import NetworkInterfaceService
+from common.infrastructure.async_client import get_async_client
+from common.infrastructure.database import get_db_session
+from common.util.auth_token_manager import get_current_user
+from common.util.compensating_transaction import compensating_transaction
+from common.util.context import CurrentUser
+
+router = APIRouter(prefix="/network-interfaces", tags=["network-interface"])
+
+
+@router.post(
+    path="/{network_interface_id}/floating-ips/{floating_ip_id}",
+    status_code=HTTP_200_OK,
+    summary="network interface 에 floating ip 연결",
+    responses={
+        401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "network interface 에 대한 접근 권한이 없는 경우"},
+        404: {"description": "자원을 찾을 수 없는 경우"},
+        409: {"description": "network interface 에 연결할 수 없는 floating ip인 경우"},
+    }
+)
+async def attach_floating_ip_to_server(
+    network_interface_id: int,
+    floating_ip_id: int,
+    current_user: CurrentUser = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db_session),
+    client: AsyncClient = Depends(get_async_client),
+    network_interface_service: NetworkInterfaceService = Depends(),
+) -> None:
+    async with compensating_transaction() as compensating_tx:
+        await network_interface_service.attach_floating_ip_to_server(
+            compensating_tx=compensating_tx,
+            session=session,
+            client=client,
+            keystone_token=current_user.keystone_token,
+            project_id=current_user.project_id,
+            floating_ip_id=floating_ip_id,
+            network_interface_id=network_interface_id,
+        )
+
+
+@router.delete(
+    path="/floating-ips/{floating_ip_id}",
+    status_code=HTTP_200_OK,
+    summary="network interface에 floating ip 연결 해제",
+    responses={
+        401: {"description": "인증 정보가 유효하지 않은 경우"},
+        403: {"description": "network interface 에 대한 접근 권한이 없는 경우"},
+        404: {"description": "자원을 찾을 수 없는 경우"},
+        409: {"description": "network interface 에 연결되어 있는 floating ip가 아닌 경우"},
+    }
+)
+async def detach_floating_ip_from_server(
+    floating_ip_id: int,
+    current_user: CurrentUser = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db_session),
+    client: AsyncClient = Depends(get_async_client),
+    network_interface_service: NetworkInterfaceService = Depends(),
+) -> None:
+    async with compensating_transaction() as compensating_tx:
+        await network_interface_service.detach_floating_ip_from_server(
+            compensating_tx=compensating_tx,
+            session=session,
+            client=client,
+            keystone_token=current_user.keystone_token,
+            project_id=current_user.project_id,
+            floating_ip_id=floating_ip_id,
+        )
