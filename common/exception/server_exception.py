9c56957dbe5ba41eb99fbf6b068cbf85df2bba28
Ted <my.koh@gabia.com>
2025-05-14 16:52:22 +0900

    feat: #48 서버 삭제 exception 추가

--- common/exception/server_exception.py
+++ common/exception/server_exception.py
@@ -35,3 +35,21 @@ class ServerNameDuplicateException(CustomException):
             status_code=409,
             message="이미 사용중인 서버 이름입니다."
         )
+
+
+class ServerDeletePermissionDeniedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SERVER_DELETE_PERMISSION_DENIED",
+            status_code=403,
+            message="해당 서버를 삭제할 수 있는 권한이 없습니다."
+        )
+
+
+class ServerDeletionFailedException(CustomException):
+    def __init__(self):
+        super().__init__(
+            code="SERVER_DELETION_FAILED",
+            status_code=500,
+            message="서버 삭제 처리 중 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+        )
